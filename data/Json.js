7056ec6afc8dd1919d08136b63a794ca93f56127
Venkat Prudhvi <dv.prudhvi@gmail.com>
2021-01-19 09:59:45 +0530

    new file:   data/Json data.json 	new file:   data/Json.js

---
+++ data/Json.js
@@ -0,0 +1,34 @@
+export const Employees = [{
+        "key": "1",
+        "userId": "krish",
+        "jobTitle": "Developer",
+        "firstName": "Krish",
+        "lastName": "Lee",
+        "employeeCode": "E1",
+        "region": "CA",
+        "phoneNumber": "123456",
+        "emailAddress": "krish.lee@learningcontainer.com"
+    },
+    {
+        "key": "2",
+        "userId": "david",
+        "jobTitle": "Developer",
+        "firstName": "Devid",
+        "lastName": "Rome",
+        "employeeCode": "E2",
+        "region": "CA",
+        "phoneNumber": "1111111",
+        "emailAddress": "devid.rome@learningcontainer.com"
+    },
+    {
+        "key": "3",
+        "userId": "tin",
+        "jobTitle": "Program Directory",
+        "firstName": "tin",
+        "lastName": "jonson",
+        "employeeCode": "E3",
+        "region": "CA",
+        "phoneNumber": "2222222",
+        "emailAddress": "tin.jonson@learningcontainer.com"
+    }
+]
